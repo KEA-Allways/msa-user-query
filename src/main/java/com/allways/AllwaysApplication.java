762daf98f8c05245b9400e450a757138bed2d7f0
minjun0707 <jmk7117@naver.com>
2023-11-01 15:47:27 +0900

    initial project

---
+++ src/main/java/com/allways/AllwaysApplication.java
@@ -0,0 +1,15 @@
+package com.allways;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AllwaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllwaysApplication.class, args);
+	}
+
+}
