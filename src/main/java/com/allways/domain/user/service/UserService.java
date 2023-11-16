c1638749a9953b72ad415201edfda1826dd5277c
minjun0707 <jmk7117@naver.com>
2023-11-16 18:04:39 +0900

    feat : queryUser service 기능 추가

--- src/main/java/com/allways/domain/user/service/UserService.java
+++ src/main/java/com/allways/domain/user/service/UserService.java
@@ -1,9 +1,14 @@
 package com.allways.domain.user.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.allways.common.exception.UserNotFoundException;
 import com.allways.domain.user.domain.User;
 import com.allways.domain.user.domain.UserDto;
 import com.allways.domain.user.domain.UserReadRequest;
+import com.allways.domain.user.dto.UserByPostRequest;
+import com.allways.domain.user.dto.UserByPostResponse;
 import com.allways.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,11 +16,12 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 
 @Service
-@Transactional
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+
+    @Transactional
     public UserDto readUser(UserReadRequest req){
         User user = userRepository.findUserByUserSeq(req.getUserSeq());
 
@@ -25,5 +31,34 @@ public class UserService {
         }
 
         return UserDto.toDto(user);
+    }
+
+
+
+    @Transactional
+    public List<UserByPostResponse> queryUser(List<UserByPostRequest> userByPostRequestList){
+
+        List<Long> userSeqList = new ArrayList<>();
+
+        for (UserByPostRequest userByPostRequest : userByPostRequestList) {
+            userSeqList.add(userByPostRequest.getUserSeq());
+        }
+
+        List<User> userList = userRepository.findUserByUserSeqIn(userSeqList);
+
+        List<UserByPostResponse> userByPostResponseList = new ArrayList<>();
+
+        for (UserByPostRequest userByPostRequest : userByPostRequestList) {
+            for (User user : userList) {
+                if(userByPostRequest.getUserSeq() == user.getUserSeq()) {
+                    userByPostResponseList.add(new UserByPostResponse(userByPostRequest.getPostSeq(),
+                        userByPostRequest.getUserSeq(),
+                        user.getUserId(), user.getNickname()));
+                }
+            }
+        }
+
+
+        return userByPostResponseList;
     }
 }
