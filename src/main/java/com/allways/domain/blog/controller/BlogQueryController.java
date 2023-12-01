395eea57371f0ca122a2dd1c591afcb88e868047
suha <hp980724@gmail.com>
2023-12-01 16:32:41 +0900

    ABF-199 merge : 합침
    ABF-199 merge : 합침

--- src/main/java/com/allways/domain/blog/controller/BlogQueryController.java
+++ src/main/java/com/allways/domain/blog/controller/BlogQueryController.java
@@ -4,10 +4,7 @@ import com.allways.common.response.Response;
 import com.allways.domain.blog.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,9 +13,10 @@ public class BlogQueryController {
     private final BlogService blogService;
 
     //유저의 블로그 정보 조회
-    @GetMapping("/api/blog/{userSeq}")
+    @GetMapping("/api/blog")
     @ResponseStatus(HttpStatus.OK)
-    public Response readBlog(@PathVariable Long userSeq){
+    public Response readBlog(@RequestHeader(value = "userSeq") Long userSeq){
+
         return Response.success(blogService.readBlog(userSeq));
     }
 }
--- src/main/java/com/allways/domain/user/entity/UserResponse.java
+++ src/main/java/com/allways/domain/user/entity/UserResponse.java
@@ -10,9 +10,12 @@ public class UserResponse {
     private Long userSeq;
     private String nickname;
     private String blogName;
+    private String password;
+    private String profileUrl;
 
     public static UserResponse toDto(User user) {
         return new UserResponse(user.getUserSeq(), user.getNickname()
-                , (user.getBlog() != null) ? user.getBlog().getBlogName() : "");
+                , (user.getBlog() != null) ? user.getBlog().getBlogName() : "",user.getProfileImgSeq(),user.getPassword());
+
     }
 }
